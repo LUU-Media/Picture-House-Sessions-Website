01df8190c463d96592bc4a17547ec944bf0efb1c
Jack Biggin <me@jackbiggin.me>
2018-04-19 16:49:33 +0100

    Homepage layout with placeholder

--- index.php
+++ index.php
@@ -7,37 +7,56 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Picture House Sessions</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.4.3/css/foundation.min.css" />
+    <link rel="stylesheet" href="./assets/css/style.css" />
+    <link href="https://fonts.googleapis.com/css?family=Merriweather:400,400i,700,700i" rel="stylesheet">
+
+
 </head>
 
 <body>
-
-    <div class="top-bar">
-        <div class="top-bar-left">
-            <ul class="dropdown menu" data-dropdown-menu>
-                <li class="menu-text">Site Title</li>
-                <li>
-                    <a href="#">One</a>
-                    <ul class="menu vertical">
-                        <li><a href="#">One</a></li>
-                        <li><a href="#">Two</a></li>
-                        <li><a href="#">Three</a></li>
+    <div class="hero-full-screen">
+        <div class="top-content-section">
+            <div class="title-bar" data-responsive-toggle="nav" data-hide-for="medium">
+                <a href="./"><img src="./assets/img/logo.png" class="nav-logo left" /></a>
+                <div class="title-bar-right">
+                    <button class="menu-icon" type="button" data-toggle></button>
+                </div>
+            </div>
+            <div class="top-bar" id="nav">
+                <div class="top-bar-left">
+                    <ul class="vertical medium-horizontal menu" data-responsive-menu="accordion medium-dropdown">
+                        <li class="menu-text show-for-medium"><a href="./"><img src="http://via.placeholder.com/300x350" alt="Picture House Sessions" /></a></li>
+                        <li><a href="./" class="nav-active">Home</a></li>
+                        <li><a href="./about">About</a></li>
+                        <li><a href="./schedule">Bands</a></li>
+                        <li><a href="./schedule">Buy</a></li>
                     </ul>
-                </li>
-                <li><a href="#">Two</a></li>
-                <li><a href="#">Three</a></li>
-            </ul>
+                </div>
+            </div>
         </div>
-        <div class="top-bar-right">
-            <ul class="menu">
-                <li><input type="search" placeholder="Search"></li>
-                <li><button type="button" class="button">Search</button></li>
-            </ul>
-        </div>
-    </div>
-    <h1>Hello, world!</h1>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/what-input/5.0.5/what-input.min.js"></script>
+        <div class="middle-content-section">
+            <img src="http://via.placeholder.com/450x600" class="hero-logo" />
+            <h1>Picture House Sessions</h1>
+            <div class="hero-subtitle">EP release: 27th April 2018</div>
+
+            <a class="button large hero-button" href="https://www.leedstickets.com/eventinfo/5524/Northern-Connection">Launch Event</a>
+            <a class="button large hero-button" href="http://cpwm.awesomedistro.com/products/611162-leeds-student-radio-come-play-with-me-presents-picture-house-sessions">Preorder</a>
+
+        </div>
+
+        <div class="bottom-content-section" data-magellan data-threshold="0">
+
+        </div>
+
+    </div>
+
+    <div class="bottom-footer">
+        <div class="bottom-footer-copyright">&copy; Picture House Sessions 2018<br />Website made with <span style="color:#ff005a">❤</span> by <a href="http://jackbiggin.me">Jack Biggin</a></div>
+    </div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/what-input/5.0.3/what-input.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.4.3/js/foundation.min.js"></script>
     <script>
         $(document).foundation();
