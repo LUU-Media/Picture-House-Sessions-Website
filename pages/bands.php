3bc3af2ef00f46f4ea517ee22beda1d1500497de
Jack Biggin <me@jackbiggin.me>
2018-04-28 05:45:15 +0100

    Placeholder bands page

---
+++ pages/bands.php
@@ -0,0 +1,32 @@
+<h2>About the Bands</h2>
+<div class="content-box">
+    <h3>Kumiko</h3>
+    <a href="https://www.facebook.com/pg/kumikoknew">Facebook Page</a>
+    <p>"The quartet, led by the beautiful vocals of Francesca Pidgeon, sprinkle jazz in to their alternative guitar sounds to make for a timid but captivating sound. There are very few bands that capture what Kumiko do and with another release anticipated in the next few months, it’s about time you got familiar with the band." - Drowned In Sound</p>
+
+    <hr />
+
+    <h3>Laminate Pet Animal</h3>
+    <a href="https://www.facebook.com/pg/laminatepetanimalmusic">Facebook Page</a>
+    <p>You may hear hints of Gold Panda, The Japanese House and Bonobo in Laminate Pet Animal; but the electronica trio from Leeds (UK), have quickly fabricated their own sound with organic samples, lush vocals and pulsing bass lines.</p>
+    <p>Having forged a reputation through remixing established artists such as Magic Man and many local bands, LPA soon found themselves getting booked to support Gold Panda, Emancipator, Slow Magic and Nathan Fake, as well as playing multiple festivals including Live At Leeds and Sound City.</p>
+    <p>'Abstract pop laced with intelligence, it fuses the digital with the organic' - Robin Murray, CLASH</p>
+
+    <hr />
+
+    <h3>Talkboy</h3>
+    <a href="https://www.facebook.com/pg/talkboyyy/">Facebook Page</a>
+
+    <hr />
+
+    <h3>Jabbawoki</h3>
+    <a href="https://www.facebook.com/Jabbawoki.Leeds">Facebook Page</a>
+    <p>Jabbawoki are a Leeds based band that formed in 2012. The group is a four piece that draw ideas from all corners of music and consists of two guitars, Finlay Vallance and Tom Cranston - on vocals, boomin bass Bendalow, Matthew Edmunds and the undisputed king of rhythm Max Lord.</p>
+    <p>Through playing together for four years Jabbawoki have developed a unique sound which encompass' elements of rap, indie and jazz</p>
+    <p>'Jabbawoki are a lovechild of Ratboy and Arctic Monkeys, with funk undertones.' - Sounds of an Eccentric</p>
+
+    <h3>The Seagulls</h3>
+    <a href="https://www.facebook.com/pg/theseagulls">Facebook Page</a>
+    <p>Modern pop troupe from Leeds possessing a sixties sound and British rock 'n' roll vibe. With their interesting vocal patterns, instantly recognisable guitar sound and lyrics touching on the weird, dark and altogether unusual, you won't be able to get these out of your head. Labelled by fans as modern day 'tropical rock'.</p>
+
+</div>
